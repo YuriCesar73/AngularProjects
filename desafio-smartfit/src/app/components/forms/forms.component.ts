35339334bd2ff8ae001f3045b5888213576053bf
Yuri Cesar <ycesar4273@gmail.com>
2024-03-08 10:19:58 -0300

    Iniciado a lógica do formulário no componente.

--- desafio-smartfit/src/app/components/forms/forms.component.ts
+++ desafio-smartfit/src/app/components/forms/forms.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
   imports: [],
+  providers: [],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
+
+  //OBS: VERIFICAR O BINDING DO FORM GROUP NO HTML DO COMPONENTE
 
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) { }
+
+
+  ngOnInit(): void {
+    this.formGroup = this.formbuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit(){
+    console.log("Submit")
+  }
+
+  onClean(){
+    console.log("clean")
+  }
 
 }
