994667d6a8c009d3771a082b4417f0fa16073752
Yuri Cesar <ycesar4273@gmail.com>
2024-04-17 16:07:27 -0300

    Criada tela de signup

--- AppLogin/login-page/src/app/app.routes.ts
+++ AppLogin/login-page/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
+        path: '*',
+        redirectTo: 'login'
+    },
+    {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
     }
 ];
