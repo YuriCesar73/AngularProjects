eea18be37a4b410287298e669d7ab5dc92685055
Yuri Cesar <ycesar4273@gmail.com>
2024-04-22 14:42:09 -0300

    Criada entidade User

---
+++ AppLogin/login-auth-api/login-auth-api/src/main/java/com/example/loginauthapi/domain/user/User.java
@@ -0,0 +1,34 @@
+package com.example.loginauthapi.domain.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+}
