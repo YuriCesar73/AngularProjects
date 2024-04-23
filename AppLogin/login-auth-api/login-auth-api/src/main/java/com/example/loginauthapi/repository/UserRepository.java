5876eb62efbb901e89c88981398ac1c3bc809dc2
Yuri Cesar <ycesar4273@gmail.com>
2024-04-23 13:41:44 -0300

    Configurações do spring security completa

--- AppLogin/login-auth-api/login-auth-api/src/main/java/com/example/loginauthapi/repository/UserRepository.java
+++ AppLogin/login-auth-api/login-auth-api/src/main/java/com/example/loginauthapi/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.loginauthapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.loginauthapi.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+	Optional<User> findByEmail(String login);
 
 }
