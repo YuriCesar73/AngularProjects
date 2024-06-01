fe1535dc18db2f0b3729f33bb54c519519911151
Yuri Cesar <ycesar4273@gmail.com>
2024-06-01 19:24:55 -0300

    Cards sendo exibidos corretamente (um ao lado do outro).

--- cardapio/src/app/components/card/card.component.ts
+++ cardapio/src/app/components/card/card.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../interfaces/Recipe';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
-  selector: 'app-card',
+  selector: 'card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
