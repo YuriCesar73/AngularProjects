8ca05f1ef6ec84a98839241314bd200d8fa4b90c
Yuri Cesar <ycesar4273@gmail.com>
2024-06-01 23:54:43 -0300

    Adicionando e removendo itens da lista de favoritos (uso do output).

--- cardapio/src/app/components/card/card.component.ts
+++ cardapio/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../interfaces/Recipe';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,20 +16,24 @@ import { MatIconModule } from '@angular/material/icon';
 export class CardComponent {
 
   @Input() food!: Recipe;
+  @Output('addFavorite') addFavorite = new EventEmitter<Recipe>();
+  @Output('removeFavorite') removeFavorite = new EventEmitter<Recipe>();
 
   favorite: boolean = false;
   iconColor: string = 'primary'
 
 
-  isFavorite(){
-    console.log("Cheguei no isFavorite.")
+
+  isFavorite(recipe: Recipe){
     if(!this.favorite){
       this.iconColor = 'warn'
       this.favorite = !this.favorite;
+      this.addFavorite.emit(recipe);
     }
     else {
       this.iconColor = 'primary'
       this.favorite = !this.favorite;
+      this.removeFavorite.emit(recipe);
     }
   }
 
--- cardapio/src/app/components/home/home.component.ts
+++ cardapio/src/app/components/home/home.component.ts
@@ -1,9 +1,8 @@
+import { Recipe } from './../../interfaces/Recipe';
 import { Component, OnInit } from '@angular/core';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { RecipeService } from '../../services/recipe.service';
-import { Observable } from 'rxjs';
-import { Recipe } from '../../interfaces/Recipe';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
@@ -20,6 +19,7 @@ import { CardComponent } from '../card/card.component';
 export class HomeComponent implements OnInit {
 
   recipes: Recipe[] = [];
+  favoriteRecipes: Recipe[] = [];
 
   constructor(private service: RecipeService) {}
 
@@ -27,5 +27,17 @@ export class HomeComponent implements OnInit {
     this.service.fetchData().subscribe((data:any) => {
       this.recipes = data.recipes; 
     })
+  }
+
+
+  addFavoriteRecipe(recipe: Recipe){
+    this.favoriteRecipes.push(recipe);
+  }
+
+  removeFavoriteRecipe(recipe: Recipe){
+    if(this.favoriteRecipes.length != 0){
+      let index = this.favoriteRecipes.findIndex((r) => r.id == recipe.id);
+      this.favoriteRecipes.splice(index, 1);
+    }
   }
 }
